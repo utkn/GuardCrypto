df72fce58b77f49258fd08a2976ec9740dd7e41b
utkn <39ph4f87a@gmail.com>
2019-09-20 14:55:15 +0800

    Cleaned up main.

--- src/gcrypto/Main.java
+++ src/gcrypto/Main.java
@@ -1,15 +1,5 @@
 package gcrypto;
 
-import it.unisa.dia.gas.jpbc.Element;
-import it.unisa.dia.gas.jpbc.Field;
-import it.unisa.dia.gas.jpbc.Pairing;
-import it.unisa.dia.gas.jpbc.PairingParametersGenerator;
-import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
-import it.unisa.dia.gas.plaf.jpbc.pairing.a.TypeACurveGenerator;
-import it.unisa.dia.gas.plaf.jpbc.pairing.parameters.PropertiesParameters;
-
-import java.math.BigInteger;
-
 public class Main {
 
     public static void main(String[] args) {
